f82ea625be4d6efd722b127e3ea8f2296ad227e6
georges <george.shaw2001@gmail.com>
2021-03-25 14:20:05 +0000

    frontEnd

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/main.scss'
+import App from './App'
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.render(<App/>, document.getElementById('root'))
