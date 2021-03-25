c42852067b104afaebea356877a5c023a050dee7
Anna Monkman <annamonkman@me.com>
2021-03-25 16:30:38 +0000

    getting data in ifestivalindex

---
+++ client/src/components/FestivalCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Card, Icon, Image } from 'semantic-ui-react'
+
+const FestivalCard = ({ name }) => {
+
+  return (
+    <>
+      <h1> hello </h1>
+      <h1>  {name} </h1>
+    </>
+  )
+
+}
+
+export default FestivalCard
---
+++ client/src/components/FestivalIndex.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import FestivalCard from './FestivalCard'
+
+const FestivalIndex = () => {
+  const [festivals, setFestivals] = useState(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('/api/festivals')
+      setFestivals(response.data)
+    }
+    getData()
+  }, [])
+
+  return (
+    <>
+      { festivals &&
+        <div>
+          { festivals.map( festival => (
+            <FestivalCard key={festival._id} {...festival} />
+          ))}
+        </div>
+      }
+    </>
+    
+  )
+}
+
+export default FestivalIndex
