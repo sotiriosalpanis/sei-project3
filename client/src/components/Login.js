f04d5b116ae9d0d0fee85f8a971fc14f67e87c9c
salpanis <salpanis@gmail.com>
2021-03-25 17:20:43 +0000

    login page wokring and connected to api endpoint

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Container, Header, Divider , Button, Form } from 'semantic-ui-react'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
+
+  const history = useHistory()
+
+  const [ formData, setFormData ] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (event) =>{
+    const newFormData = { ...formData, [event.target.name]: event.target.value }
+    setFormData(newFormData)
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    
+    try {
+      const response = await axios.post('/api/login',formData)
+      const token = response.data.token
+      window.localStorage.setItem('token', token)
+      history.push('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div>
-      
+      <Container>
+        <Header as='h3'>Login to your account</Header>
+        <Divider />
+        <Form className='ui form' onSubmit={handleSubmit} >
+          <div className="field">
+            <Form.Field>
+              <label>Email address:</label>
+              <input 
+                placeholder='someone@example.com' 
+                type="email"
+                name='email'
+                value={formData.email} 
+                onChange={handleChange}
+              />
+            </Form.Field>
+          </div>
+          <div className="field">
+            <Form.Field>
+              <label>Password</label>
+              <input 
+                placeholder='Choose a password' 
+                type="password"
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </Form.Field>
+          </div>
+
+          <Button type='submit' className="ui button">Login</Button>
+        </Form>
+      </Container>
     </div>
   )
 }
