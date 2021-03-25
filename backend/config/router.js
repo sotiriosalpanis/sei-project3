9eb260da996bbadde5ccf37213e283dcd67486d5
salpanis <salpanis@gmail.com>
2021-03-25 11:02:05 +0000

    Update route for festivals added

--- backend/config/router.js
+++ backend/config/router.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllFestivals, getOneFestival, addFestival } from '../controllers/festivals.js'
+import { getAllFestivals, getOneFestival, addFestival, updateFestival } from '../controllers/festivals.js'
 // import { registerUser, loginUser } from '../controllers/auth.js'
 // import { getUserProfile } from '../controllers/users.js'
 // import { secureRoute } from '../config/secureRoute.js'
@@ -8,11 +8,12 @@ const router = express.Router()
 
 router.route('/festivals')
   .get(getAllFestivals)
-  .post(addFestival)
+  .post(addFestival) //! Building without secure route- will need to be added later
   // .post(secureRoute, addFestival)
 
 router.route('/festivals/:id')
   .get(getOneFestival)
+  .put(updateFestival) //! Building without secure route- will need to be added later
   // .put(secureRoute, updateFestival)
   // .delete(secureRoute, deleteFestival)
 
--- backend/controllers/festivals.js
+++ backend/controllers/festivals.js
@@ -9,6 +9,17 @@ export const getAllFestivals = async (_req, res) => {
   return res.status(200).json(festivals)
 }
 
+export const addFestival = async (req, res) => {
+  try {
+    const festivalToAdd = await Festival.create(req.body)
+    return res.status(201).json(festivalToAdd)
+  } catch (err) {
+    console.log('🥴 could not add the festival')
+    console.log(err)
+    return res.status(422).json(err)
+  }
+}
+
 export const getOneFestival = async (req, res) => {
   try{
     const { id } = req.params
@@ -23,13 +34,18 @@ export const getOneFestival = async (req, res) => {
   }
 }
 
-export const addFestival = async (req, res) => {
+
+export const updateFestival = async (req, res ) => {
   try {
-    const festivalToAdd = await Festival.create(req.body)
-    return res.status(201).json(festivalToAdd)
+    const { id } = req.params
+    const festivalToUpdate = await Festival.findById(id)
+    if (!festivalToUpdate) throw new Error('Could not find festival to PUT info into')
+    Object.assign(festivalToUpdate,req.body)
+    await festivalToUpdate.save()
+    return res.status(202).json(festivalToUpdate)
   } catch (err) {
-    console.log('🥴 could not add the festival')
+    console.log('🥴 could not PUT info into the festival')
     console.log(err)
-    return res.status(422).json(err)
+    return res.status(404).json(err)
   }
 }
