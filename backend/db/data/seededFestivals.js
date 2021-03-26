0acb0fbb022db6eab1c775385ab0b86c7f0a9433
georges <george.shaw2001@gmail.com>
2021-03-26 12:06:39 +0000

    more work on Home

--- backend/db/data/seededFestivals.js
+++ backend/db/data/seededFestivals.js
@@ -64,7 +64,7 @@ export default [
     country: 'China',
     latitude: 40.44552,
     longitude: 116.66352,
-    mainFestivalImage: '../../assets/mainFestivalImage/great-wall-main.png'
+    mainFestivalImage: 'https://imgur.com/bfVwBTr'
   },
   {
     festivalName: 'Primavera Sound',
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,21 @@
-import React, { setInterval, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image } from 'semantic-ui-react'
 import axios from 'axios'
 
 const Home = () => {
-  const [pictures, setPictures] = useState([])
-  const [dataSet, setDataSet] = useState([])
+  
+  let pictures = []
+  let currentPicture = ''
+  const [dataSet, setDataSet] = useState(null)
+  // const [currentPic, setCurrentPic] = useState(String)
+
   useEffect(()=>{
     const getPictures = async () => {
       try {
         const response = await axios.get('/api/festivals')
         console.log('HERE IS THE DATA>>', response)
-        
         setDataSet(response.data)
+        
       } catch (err) {
         console.log('ERROR IN HOME')
         console.log(err)
@@ -19,26 +23,39 @@ const Home = () => {
     }
     getPictures()
   }, [])
+  
 
-  if (!dataSet) {
-    console.log('no data')
-  } else {
-    setPictures(dataSet.map(pic =>{
-      return pic.mainFestivalImage
-    }))
+  
+  const changePicture = () => {
+    if (pictures.length > 0){
+      //currentPicture = (pictures[Math.round(Math.random(0, pictures.length - 1))])
+      console.log('IN CHANGE PIC>>', pictures)
+      currentPicture = pictures[0].mainFestivalImage
+    } else {
+      currentPicture = ('https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZmVzdGl2YWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80')
+    }
+  }
+
+  const startPictureChange = () => {
+    setTimeout(() => changePicture(), 1000)
   }
   
-  setInterval()
-
-  if (!pictures) return
+  startPictureChange()
+  if (!dataSet) return null
+  const temp = dataSet.map(pic => {
+    return pic.mainFestivalImage
+  })
+  pictures = temp
+  console.log('CURRENT PIC',currentPicture)
 
   return (
     <main>
       <div className='Container '>
+        <h1>{dataSet[0].festivalName}</h1>
         <Image 
-          src='https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZmVzdGl2YWx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+          src={currentPicture}
           fluid >
-        </Image>
+        </Image> 
       </div>
       <div> {/*Display festivals near you / popular festivals */}
 
