50eb1a004ce0825f49535cb454191f43915d4e71
Anna Monkman <annamonkman@me.com>
2021-03-29 16:26:15 +0100

    countries filter almost fin

--- client/src/components/FestivalCard.js
+++ client/src/components/FestivalCard.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import { Card, Image } from 'semantic-ui-react'
+// import { Link } from 'react-router-dom'
 
 
 
-const FestivalCard = ({ festivalName, mainFestivalImage, venue, country, startDate, endDate }) => {
-  
+const FestivalCard = ({ _id, festivalName, mainFestivalImage, venue, country, startDate, endDate }) => {
+
+  const startDateString = new Date(startDate).toDateString()
+  const endDateString = new Date(endDate).toDateString()
+
   return (
     <Card>
-      <Image src={`${mainFestivalImage}`} wrapped ui={false} />
+      <Image 
+        src={`${mainFestivalImage}`} 
+        wrapped ui={false}
+        as='a' 
+        href={`/festivals/${_id}`}
+      />
       <Card.Content>
         <Card.Header>{festivalName}</Card.Header>
         <Card.Description>
           {venue} - {country}
         </Card.Description>
         <Card.Description>
-          {startDate} - {endDate}
+          {startDateString} - {endDateString}
         </Card.Description>
       </Card.Content>
     </Card>
--- client/src/components/FestivalIndex.js
+++ client/src/components/FestivalIndex.js
@@ -8,30 +8,71 @@ import FestivalCard from './FestivalCard'
 
 const FestivalIndex = () => {
   const [festivals, setFestivals] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
+  const [filterValue, setFilterValue] = useState('')
+  console.log('set filter value', setFilterValue)
 
-  const countryOptions = [
-    { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
-    { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
-    { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
-    { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
-    { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
-    { text: `${festivals.country}` }
-  ]
+  // const countryOptions = [
+  //   { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
+  //   { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
+  //   { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
+  //   { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
+  //   { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
+  //   { text: `${countries.country}` }
+  // ]
+
+
+
+  // const countryOptions = festivals.map((festival) => {
+  //   <option key={festival._id}>{festival.country}</option>
+  // })
+  // console.log('countryoptions>> ', countryOptions)
 
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get('/api/festivals')
       setFestivals(response.data)
-      console.log('FESTIVALS country>>', response.data.country)
     }
     getData()
 
   }, [])
+
+  const handleChange = (event) => {
+    const filteredCountry = event.target.value
+    console.log('ETV', filteredCountry)
+    console.log('filtered countries>', filteredCountries)
+    setFilterValue(filteredCountry)
+
+  }
+
+  useEffect((event) => {
+    if (!filterValue || filterValue === 'All') return (
+      setFilteredCountries(festivals)
+    )
+
+    console.log('event', event)
+    const filteredFest = festivals.filter(festival => {
+      return festival.country === filterValue
+    })
+    console.log('FILTEREDARRAY>>', filteredFest)
+    setFilteredCountries(filteredFest)
+  }, [filterValue, festivals])
   
+  let countries = festivals.map(festival => {
+    return festival.country
+  })
+  console.log('countries', countries)
 
+  const countriesSet = new Set(countries)
+  console.log('countriesSet>>>', countriesSet)
 
+  countries = [...countriesSet]
+  console.log('COUNTRIES>>>>>', countries)
 
-  
+  // * semantic UI countries filter
+  const countriesOptions = countries.map(country => {
+    return { text: `${country}` }
+  })
 
   if (!festivals) return null
   return (
@@ -44,14 +85,30 @@ const FestivalIndex = () => {
           multiple
           search
           selection
-          options={countryOptions}
+          options={countriesOptions}
           placeholder='Select Country'
         />
+
+        <select name="filter" id="filter" onChange={handleChange}>
+          <option value='All'>All</option>
+          {countries.map(country => (
+            <option key={country} 
+              value={country}>
+              {country}
+            </option>
+          ))}
+
+          {/* {festivals.map(festival => (
+            <option key={festival._id}
+              value={festival.country}
+            >{festival.country}</option>
+          ))} */}
+        </select>
       </div>
 
       <Grid centered stackable>
         <Grid.Row columns={4}>
-          { festivals.map( festival => {
+          { filteredCountries.map( festival => {
             return <Grid.Column key={festival._id}>
               <FestivalCard {...festival} />
             </Grid.Column> 
