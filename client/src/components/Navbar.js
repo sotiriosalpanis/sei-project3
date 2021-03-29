deeab350b7de5b07d61558d64327be41100634b1
salpanis <salpanis@gmail.com>
2021-03-29 11:15:20 +0100

    Added conditional redering for logged in or out

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { Menu, Dropdown } from 'semantic-ui-react'
+import { userIsAuthenticated } from './helpers/auth'
 
 
 
 const Navigation = () => {
+
+  const history = useHistory()
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('token')
+    setIsLoggedIn(false)
+    history.push('/sign-in')
+  }
+
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+
+  useEffect(() => {
+    if (userIsAuthenticated()) return setIsLoggedIn(true)
+  },[isLoggedIn])
+
+
+
+  console.log('Is Logged in?>>>>',isLoggedIn)
+  console.log('Is Authenticated?>>>>',userIsAuthenticated())
 
 
   return (
@@ -30,16 +50,43 @@ const Navigation = () => {
             >
               Map of festivals
             </Dropdown.Item>
+            { isLoggedIn && 
+              <Dropdown.Item
+                as= { Link }
+                to='/my-festivals'
+                name='My Festivals'
+              >
+              My Festivals
+              </Dropdown.Item>
+            
+            }
+
+
           </Dropdown.Menu>
 
         </Dropdown>
 
         <Menu.Menu position='right'>
+          { !isLoggedIn &&
           <Menu.Item
             as= { Link }
             to='/sign-in'
             name='Account'
           />
+          }
+          { isLoggedIn &&
+          <>
+            <Menu.Item
+              as= { Link }
+              to='/userprofile'
+              name='Your profile'
+            />
+            <Menu.Item 
+              name='Logout'
+              onClick={ handleLogout }
+            />
+          </>
+          }
         </Menu.Menu>
       </Menu>
     </header>
