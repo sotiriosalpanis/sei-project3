55cf59ef014b7bc781c03bc0500a44b1a3519c15
Anna Monkman <annamonkman@me.com>
2021-03-30 11:17:10 +0100

    adding more filters

--- client/src/components/FestivalIndex.js
+++ client/src/components/FestivalIndex.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Grid, Dropdown, Pagination } from 'semantic-ui-react'
+import { Grid, Dropdown, Pagination, Button } from 'semantic-ui-react'
 
 import FestivalCard from './FestivalCard'
 
@@ -8,26 +8,27 @@ import FestivalCard from './FestivalCard'
 
 const FestivalIndex = () => {
   const [festivals, setFestivals] = useState([])
-  const [filteredCountries, setFilteredCountries] = useState([])
+  // const [filteredCountries, setFilteredCountries] = useState([])
+  const [filteredFestivals, setFilteredFestivals] = useState([])
   const [filterValue, setFilterValue] = useState('')
-  console.log('set filter value', setFilterValue)
 
+  // * Semantic UI example
   // const countryOptions = [
   //   { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
-  //   { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
-  //   { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
-  //   { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
-  //   { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
-  //   { text: `${countries.country}` }
+  //   { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' }
   // ]
 
-
+  // const options = [
+  //   { key: 'angular', text: 'Angular', value: 'angular' },
+  //   { key: 'css', text: 'CSS', value: 'css' },
+  //   { key: 'design', text: 'Graphic Design', value: 'design' },
 
   // const countryOptions = festivals.map((festival) => {
   //   <option key={festival._id}>{festival.country}</option>
   // })
   // console.log('countryoptions>> ', countryOptions)
 
+  // * GET DATA
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get('/api/festivals')
@@ -37,49 +38,75 @@ const FestivalIndex = () => {
 
   }, [])
 
-  const handleChange = (event) => {
-    const filteredCountry = event.target.value
-    console.log('ETV', filteredCountry)
-    console.log('filtered countries>', filteredCountries)
+  // * HANDLE CHANGE COUNTRIES
+  const handleChangeCountries = (event) => {
+    const filteredCountry = event.target.innerText
+    console.log('ETV COUNTRY>>', filteredCountry)
     setFilterValue(filteredCountry)
-
   }
 
+  // * HANDLE CHANGE OTHER
+  const handleChange = (event) => {
+    const filteredFestival = event.target.value
+    console.log('ETV FILTEREDFEST>', filteredFestival)
+    setFilterValue(filteredFestival)
+  }
+
+  // * RETURN FILTERED FESTIVALS
   useEffect((event) => {
     if (!filterValue || filterValue === 'All') return (
-      setFilteredCountries(festivals)
+      setFilteredFestivals(festivals)
     )
-
     console.log('event', event)
     const filteredFest = festivals.filter(festival => {
-      return festival.country === filterValue
+      return festival.country === filterValue || festival.lineup === filterValue
     })
-    console.log('FILTEREDARRAY>>', filteredFest)
-    setFilteredCountries(filteredFest)
+    setFilteredFestivals(filteredFest)
   }, [filterValue, festivals])
   
+
+  // * SEMANTIC UI COUNTRIES FILTER
   let countries = festivals.map(festival => {
     return festival.country
   })
-  console.log('countries', countries)
-
   const countriesSet = new Set(countries)
-  console.log('countriesSet>>>', countriesSet)
-
   countries = [...countriesSet]
-  console.log('COUNTRIES>>>>>', countries)
-
-  // * semantic UI countries filter
   const countriesOptions = countries.map(country => {
-    return { text: `${country}` }
+    return { key: `${country}`, text: `${country}`, value: `${country}`, flag: `${country.toLowerCase()}` }
   })
 
+  // * SEMANTIC UI ARTISTS FILTER
+  let artists = festivals.map(festival => {
+    return festival.lineup
+  })
+  const artistsSet = new Set(artists)
+  artists = [...artistsSet]
+  const artistsOptions = artists.map(artist => {
+    return { key: `${artist}`, text: `${artist}`, value: `${artist}` }
+  })
+
+  // * SEMANTIC UI PRICE FILTER
+  const priceOptions = [
+    { key: 'cheap', text: 'under £50', value: 'cheap' },
+    { key: 'midOne', text: '£50 - £100', value: 'midOne' },
+    { key: 'midTwo', text: '£100-200', value: 'midTwo' },
+    { key: 'expensive', text: 'over £200', value: 'expensive' }
+  ]
+
+
+
+  // * IF NO FESTIVALS
   if (!festivals) return null
+
+  // * RETURN
   return (
 
     <>
-      <div className="filter-container">
+      <div className="filters-container">
+
+        { /* Semantic UI Countries*/ }
         <Dropdown
+          onChange={handleChangeCountries}
           clearable
           fluid
           multiple
@@ -87,9 +114,10 @@ const FestivalIndex = () => {
           selection
           options={countriesOptions}
           placeholder='Select Country'
+          // value={countriesOptions}
         />
 
-        <select name="filter" id="filter" onChange={handleChange}>
+        <select name="filter" id="filter" onChange={handleChangeCountries}>
           <option value='All'>All</option>
           {countries.map(country => (
             <option key={country} 
@@ -97,18 +125,31 @@ const FestivalIndex = () => {
               {country}
             </option>
           ))}
+        </select>
 
-          {/* {festivals.map(festival => (
+        {/* {festivals.map(festival => (
             <option key={festival._id}
               value={festival.country}
             >{festival.country}</option>
           ))} */}
-        </select>
+        
+
+        { /* Semantic UI Artists */ }
+        <Dropdown placeholder='Artists' fluid multiple selection options={artistsOptions} onChange={handleChange}/>
+
+        { /* Semantic UI Price */ }
+        <Dropdown placeholder='Price per day (£)' fluid multiple selection options={priceOptions} onChange={handleChange}/>
+
+
+        
+
+        <Button>Submit</Button>
+        <Button>Reset</Button>
       </div>
 
       <Grid centered stackable>
         <Grid.Row columns={4}>
-          { filteredCountries.map( festival => {
+          { filteredFestivals.map( festival => {
             return <Grid.Column key={festival._id}>
               <FestivalCard {...festival} />
             </Grid.Column> 
