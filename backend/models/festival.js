95bc69b4405a776f346815463af60b12976de357
Awyssa <michael.ehson@gmail.com>
2021-03-30 08:03:42 +0100

    added comment schema

--- backend/models/festival.js
+++ backend/models/festival.js
@@ -4,10 +4,18 @@ const festivalImageSchema = new mongoose.Schema({
   userImages: [{ type: String }]
 })
 
-const festivalAttendanceSchema = new mongoose.Schema({
-  interested: { type: Boolean, default: false },
-  going: { type: Boolean, default: false },
-  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+// const festivalAttendanceSchema = new mongoose.Schema({
+//   interested: { type: Boolean, default: false },
+//   going: { type: Boolean, default: false },
+//   user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+// })
+
+const commentSchema = new mongoose.Schema({
+  text: { type: String, maxlength: 300 },
+  attending: { type: Boolean },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamp: true
 })
 
 const festivalSchema = new mongoose.Schema({
@@ -24,7 +32,7 @@ const festivalSchema = new mongoose.Schema({
   longitude: { type: Number, required: true },
   mainFestivalImage: { type: String, required: true },
   userImages: { festivalImageSchema },
-  festivalAttendance: [festivalAttendanceSchema]
+  comment: [commentSchema]
 })
 
 export default mongoose.model('Festival', festivalSchema)
