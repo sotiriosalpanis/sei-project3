b84f80f9132180dc2169e88c48bf37b954630490
Awyssa <michael.ehson@gmail.com>
2021-03-30 21:05:38 +0100

    merging with dev

--- backend/models/artist.js
+++ backend/models/artist.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-// Need to populate this with each festival they are attending. use a populate? Or every artists is a referance to the festival schema and each artist is created via that schema. Might be better to do this on the front end, interate over each festival.lineup then sort/set to display each artists and each festival they are attending.
+// Michael note to self: Need to populate this with each festival they are attending. use a populate? Or every artists is a referance to the festival schema and each artist is created via that schema. Might be better to do this on the front end, interate over each festival.lineup then sort/set to display each artists and each festival they are attending.
 
 const artistSchema = new mongoose.Schema({
   artist: { type: String, required: true, unique: true },
