c59e9f22ba700f60f8724cfc760c86c658ced3f3
salpanis <salpanis@gmail.com>
2021-03-30 06:59:22 +0100

    Resolved bug- users can add attendance to a festival

--- backend/controllers/festivals.js
+++ backend/controllers/festivals.js
@@ -96,17 +96,14 @@ export const addAttendanceToFestival = async (req, res) => {
     if (!festival) throw new Error('Could not find festival')
     const newAttendanceInfo = { ...req.body, user: req.currentUser._id }
     const attendeeMatch = festival.festivalAttendance.filter((fest, index) => {
-      const check = String(fest.user) === String(req.currentUser._id)
-      console.log('Check', check)
-      return index
+      return String(fest.user) === String(req.currentUser._id)
     }
     )
+    const resultIndex = festival.festivalAttendance.indexOf(attendeeMatch[0])
     if (attendeeMatch.length === 0) {
-      console.log('ATTENDEE', newAttendanceInfo)
       festival.festivalAttendance.push(newAttendanceInfo)
     } else {
-      console.log('No', newAttendanceInfo)
-      festival.festivalAttendance[attendeeMatch] = { ...newAttendanceInfo }
+      festival.festivalAttendance[resultIndex] = { ...newAttendanceInfo }
     }
     await festival.save()
     return res.status(200).json(festival)
