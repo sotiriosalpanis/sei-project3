d03938ba6b16730caff30e89e7a0c069967995c3
Awyssa <michael.ehson@gmail.com>
2021-03-30 10:36:23 +0100

    added seeded artists

--- backend/db/data/seededArtists.js
+++ backend/db/data/seededArtists.js
@@ -1,10 +1,398 @@
 export default [
   {
-    artist: 'OR:LA',
-    genre: 'House Techno'
+    artist: 'A$AP Rocky',
+    festivals: ['Sziget Festival']
   },
   {
-    artist: 'Avalon Emerson',
-    genre: 'Techno'
+    artist: 'Afrojack',
+    festivals: ['Ultra Europe']
+  },
+  {
+    artist: 'AJ Tracey',
+    festivals: ['Parklife']
+  },
+  {
+    artist: 'Alt-J',
+    festivals: ['NOS Alive']
+  },
+  {
+    artist: 'Amelie Lens',
+    festivals: ['Tomorrowland', 'Awakenings Festival']
+  },
+  {
+    artist: 'Armin van Buuren',
+    festivals: ['Ultra Europe']
+  },
+  {
+    artist: 'Becky Hill',
+    festivals: ['Highest Point Festival']
+  },
+  {
+    artist: 'Belako',
+    festivals: ['South By South West']
+  },
+  {
+    artist: 'Ben Klock',
+    festivals: ['Great Wall Festival']
+  },
+  {
+    artist: 'Beverley Knight',
+    festivals: ['Cambridge Club']
+  },
+  {
+    artist: 'Billie Eilish',
+    festivals: ['We Love 90s In The Park!']
+  },
+  {
+    artist: 'Billy Ocean',
+    festivals: ['Rewind South']
+  },
+  {
+    artist: 'Calvin Harris',
+    festivals: ['Coachella', 'Sziget Festival']
+  },
+  {
+    artist: 'Captain Sensible',
+    festivals: ['Rewind South']
+  },
+  {
+    artist: 'Carl Cox',
+    festivals: ['Awakenings Festival']
+  },
+  {
+    artist: 'Catfish And The Bottlemen',
+    festivals: ['Leeds Festival', 'Reading Festival']
+  },
+  {
+    artist: 'Celeste',
+    festivals: ['Parklife']
+  },
+  {
+    artist: 'Dababy',
+    festivals: ['Reading Festival']
+  },
+  {
+    artist: 'Dave',
+    festivals: ['Parklife']
+  },
+  {
+    artist: 'David Guetta',
+    festivals: ['Ultra Europe', 'Tomorrowland']
+  },
+  {
+    artist: 'Deftones',
+    festivals: ['Rock am Ring & Rock im Park']
+  },
+  {
+    artist: 'Dimitri Vegas & Like Mike',
+    festivals: ['Tomorrowland']
+  },
+  {
+    artist: 'DJ Snake',
+    festivals: ['Ultra Europe']
+  },
+  {
+    artist: 'Doja Cat',
+    festivals: ['Reading Festival']
+  },
+  {
+    artist: 'Dua Lipa',
+    festivals: ['Sziget Festival']
+  },
+  {
+    artist: 'Echo & The Bunnymen',
+    festivals: ['LakeFest']
+  },
+  {
+    artist: 'EKKAH',
+    festivals: ['CloseUp Festival']
+  },
+  {
+    artist: 'Fat White Family',
+    festivals: ['Bigfoot Festival']
+  },
+  {
+    artist: 'FKA Twigs',
+    festivals: ['Coachella']
+  },
+  {
+    artist: 'Flume',
+    festivals: ['Coachella']
+  },
+  {
+    artist: 'Foo Fighters',
+    festivals: ['New Orleans Jazz & Heritage Festival']
+  },
+  {
+    artist: 'Frank Ocean',
+    festivals: ['Coachella']
+  },
+  {
+    artist: 'Frank Turner',
+    festivals: ['Black Deer Festival']
+  },
+  {
+    artist: 'George Ezra',
+    festivals: ['Latitude Festival']
+  },
+  {
+    artist: 'Gerry Cinnamon',
+    festivals: ['Leeds Festival']
+  },
+  {
+    artist: 'Gorillaz',
+    festivals: ['Splendour in the Grass', 'Primavera Sound']
+  },
+  {
+    artist: 'Green Day',
+    festivals: ['Rock am Ring & Rock im Park']
+  },
+  {
+    artist: 'Gregory Porter',
+    festivals: ['Kite Festival']
+  },
+  {
+    artist: 'Hot Chip',
+    festivals: ['Bigfoot Festival']
+  },
+  {
+    artist: 'Hue and Cry',
+    festivals: ['Rewind South']
+  },
+  {
+    artist: 'Jack Savoretti',
+    festivals: ['Cambridge Club']
+  },
+  {
+    artist: 'Jamie xx',
+    festivals: ['All Points East', 'Parklife']
+  },
+  {
+    artist: 'Kano',
+    festivals: ['All Points East']
+  },
+  {
+    artist: 'Keane',
+    festivals: ['Live In The Wyldes']
+  },
+  {
+    artist: 'Kings Of Leon',
+    festivals: ['Sziget Festival']
+  },
+  {
+    artist: 'KSI',
+    festivals: ['Leeds Festival']
+  },
+  {
+    artist: 'Lana Del Rey',
+    festivals: ['Coachella']
+  },
+  {
+    artist: 'Lewis Capaldi',
+    festivals: ['Leeds Festival', 'Reading Festival', 'We Love 90s In The Park!']
+  },
+  {
+    artist: 'Liam Gallagher',
+    festivals: ['Leeds Festival', 'Reading Festival', 'We Love 90s In The Park!']
+  },
+  {
+    artist: 'Little Simz',
+    festivals: ['Parklife', 'All Points East']
+  },
+  {
+    artist: 'Loyle Carner',
+    festivals: ['Latitude Festival']
+  },
+  {
+    artist: 'Major Lazer',
+    festivals: ['Sziget Festival']
+  },
+  {
+    artist: 'Margot Price',
+    festivals: ['South By South West']
+  },
+  {
+    artist: 'Maribou State',
+    festivals: ['Bigfoot Festival']
+  },
+  {
+    artist: 'Marshmello',
+    festivals: ['Ultra Europe']
+  },
+  {
+    artist: 'Martin Garrix',
+    festivals: ['Tomorrowland']
+  },
+  {
+    artist: 'Mavis Staples',
+    festivals: ['Kite Festival']
+  },
+  {
+    artist: 'Nick Helm',
+    festivals: ['Latitude Festival']
+  },
+  {
+    artist: 'Olly Murs',
+    festivals: ['Live In The Wyldes']
+  },
+  {
+    artist: 'Pa Salieu',
+    festivals: ['Parklife']
+  },
+  {
+    artist: 'Post Malone',
+    festivals: ['Leeds Festival']
+  },
+  {
+    artist: 'Primal Scream',
+    festivals: ['Latitude Festival', 'Bigfoot Festival']
+  },
+  {
+    artist: 'Queens Of The Stone Age',
+    festivals: ['Leeds Festival']
+  },
+  {
+    artist: 'Rag N Bone Man',
+    festivals: ['Highest Point Festival', 'Live In The Wyldes']
+  },
+  {
+    artist: 'Rage Against the Machine',
+    festivals: ['Coachella']
+  },
+  {
+    artist: 'Razorlight',
+    festivals: ['LakeFest']
+  },
+  {
+    artist: 'Red Hot Chili Peppers',
+    festivals: ['NOS Alive']
+  },
+  {
+    artist: 'Rewind South',
+    festivals: ['Rewind South']
+  },
+  {
+    artist: 'Richie Hawtin',
+    festivals: ['Awakenings Festival', 'Ultra Europe']
+  },
+  {
+    artist: 'Rudimental',
+    festivals: ['Highest Point Festival']
+  },
+  {
+    artist: 'Sad Boys Club',
+    festivals: ['CloseUp Festival']
+  },
+  {
+    artist: 'Saving Grace',
+    festivals: ['Saving Grace']
+  },
+  {
+    artist: 'Seasick Steve',
+    festivals: ['LakeFest']
+  },
+  {
+    artist: 'Slaves',
+    festivals: ['Latitude Festival']
+  },
+  {
+    artist: 'Slowthai',
+    festivals: ['All Points East']
+  },
+  {
+    artist: 'Snow Patrol',
+    festivals: ['Latitude Festival']
+  },
+  {
+    artist: 'Soccer Mommy',
+    festivals: ['South By South West']
+  },
+  {
+    artist: 'Sophie And The Giants',
+    festivals: ['CloseUp Festival']
+  },
+  {
+    artist: 'Sophie Ellis-Bextor',
+    festivals: ['Cambridge Club']
+  },
+  {
+    artist: 'Stevie Nicks',
+    festivals: ['New Orleans Jazz & Heritage Festival']
+  },
+  {
+    artist: 'Stormzy',
+    festivals: ['Leeds Festival', 'Reading Festival', 'Sziget Festival', 'We Love 90s In The Park!']
+  },
+  {
+    artist: 'System of a Down',
+    festivals: ['Rock am Ring & Rock im Park']
+  },
+  {
+    artist: 'Tame Impala',
+    festivals: ['Primavera Sound']
+  },
+  {
+    artist: 'Taylor Swift',
+    festivals: ['We Love 90s In The Park!']
+  },
+  {
+    artist: 'team Down',
+    festivals: ['South By South West']
+  },
+  {
+    artist: 'The Lumineers',
+    festivals: ['NOS Alive']
+  },
+  {
+    artist: 'The Strokes',
+    festivals: ['NOS Alive', 'Primavera Sound', 'Sziget Festival']
+  },
+  {
+    artist: 'The Who',
+    festivals: ['New Orleans Jazz & Heritage Festival']
+  },
+  {
+    artist: 'The Wombats',
+    festivals: ['Reading Festival']
+  },
+  {
+    artist: 'This Is The Kit',
+    festivals: ['Kite Festival']
+  },
+  {
+    artist: 'Tom Grennan',
+    festivals: ['Latitude Festival']
+  },
+  {
+    artist: 'Tom Mish',
+    festivals: ['All Points East']
+  },
+  {
+    artist: 'Travis Scott',
+    festivals: ['Coachella', 'We Love 90s In The Park!']
+  },
+  {
+    artist: 'Trevor Horn',
+    festivals: ['Rewind South']
+  },
+  {
+    artist: 'Two Door Cinema Club',
+    festivals: ['Reading Festival']
+  },
+  {
+    artist: 'Tyler, the Creator',
+    festivals: ['Primavera Sound']
+  },
+  {
+    artist: 'Van Morrison',
+    festivals: ['Black Deer Festival']
+  },
+  {
+    artist: 'Volbeat',
+    festivals: ['Rock am Ring & Rock im Park']
+  },
+  {
+    artist: 'Wilkinson',
+    festivals: ['Highest Point Festival']
   }
 ]
