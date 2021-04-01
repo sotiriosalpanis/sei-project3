02ed5430b01275014daa617f06241452ccea9d95
Anna Monkman <annamonkman@me.com>
2021-04-01 10:37:31 +0100

    festival card styling

--- client/src/components/FestivalCard.js
+++ client/src/components/FestivalCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Image, Container } from 'semantic-ui-react'
 
-const FestivalCard = ({ _id, festivalName, mainFestivalImage, venue, country, startDate, endDate }) => {
+const FestivalCard = ({ _id, festivalName, venue, mainFestivalImage, country, startDate, endDate }) => {
 
   const startDateString = new Date(startDate).toDateString()
   const endDateString = new Date(endDate).toDateString()
@@ -13,7 +13,30 @@ const FestivalCard = ({ _id, festivalName, mainFestivalImage, venue, country, st
         as='a' 
         href={`/festivals/${_id}`}
       >
-        <div className="festival-card-image-wrapper"></div>
+        <Container
+            style={{
+              backgroundImage: `url(${mainFestivalImage})`
+            }}
+            as='a'
+            href={`/festivals/${_id}`}
+            className="festival-card-image-wrapper">
+              <div className="festival-card-title">
+                {festivalName.toUpperCase()}
+              </div>
+        </Container>
+
+        {/* <div className="festival-card-image-wrapper">
+          style={{
+              backgroundImage: `url(${mainFestivalImage})`
+            }}
+            <span>{festivalName}</span>
+        </div> */}
+
+        <div className="festival-card-info-wrapper">
+          <p className="festival-card-venue">{venue.toUpperCase()} - {country.toUpperCase()}</p>
+          <p className="festival-card-dates">{startDateString} - {endDateString}</p>
+        </div>
+
       </Card>
     </>
 
