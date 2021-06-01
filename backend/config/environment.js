c70793f7869e2ce3b6c4e6e7f1568b854264c884
Anna Monkman <annamonkman@me.com>
2021-06-02 00:48:17 +0100

    added readme and prep for deployment but not working

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -1,3 +1,9 @@
-export const port = 4000
-export const dbURI = 'mongodb://localhost/sei-project3'
-export const secret = 'Alright then, keep your secrets'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/sei-project3'
+const port = process.env.PORT || 4000
+const secret = process.env.SECRET || 'Alright then, keep your secrets'
+
+module.exports = {
+  dbURI,
+  port,
+  secret
+}
